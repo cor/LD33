4f195fd7056e4fb11d79802fe57d483c298b3fb4
Jurriaan Pruijs <jurriaan@pruijs.nl>
2015-08-22 11:31:31 +0200

    Add AI to human

--- Assets/Scripts/HumanController.cs
+++ Assets/Scripts/HumanController.cs
@@ -10,12 +10,30 @@ public class HumanController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GameObject closestHouse = FindClosestHouse ();
+
+		transform.position = Vector2.MoveTowards(transform.position, closestHouse.transform.position, 1 * Time.deltaTime);
 	}
 
-
 	void OnCollisionEnter2D (Collision2D col) {
-		Debug.Log ("hi");
+		Debug.Log ("OnCollisionEnter2D");
 		Destroy(gameObject);
+	}
+
+	GameObject FindClosestHouse() {
+		GameObject[] gos;
+		gos = GameObject.FindGameObjectsWithTag("House");
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach (GameObject go in gos) {
+			Vector3 diff = go.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance) {
+				closest = go;
+				distance = curDistance;
+			}
+		}
+		return closest;
 	}
 }
