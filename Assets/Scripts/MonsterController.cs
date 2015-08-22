2ab375ad20b2742fb022aa330730183ba4eb9cb9
cor <cor@pruijs.nl>
2015-08-22 10:31:00 +0200

    Add MonsterController.cs

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour {
+
+	public float moveSpeed = 0.10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalIncrement = Input.GetAxis("Horizontal") * moveSpeed;
+		float verticalIncrement = Input.GetAxis("Vertical") * moveSpeed;
+
+		transform.position = new Vector2(transform.position.x + horizontalIncrement, transform.position.y + verticalIncrement);
+	
+	}
+}
