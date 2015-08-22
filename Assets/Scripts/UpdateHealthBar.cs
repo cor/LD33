15f4869b62d77ecd21c6cacef60f51a908b12f4e
cor <cor@pruijs.nl>
2015-08-22 13:11:58 +0200

    Add floating healthbar above Human's

---
+++ Assets/Scripts/UpdateHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateHealthBar : MonoBehaviour {
+
+	public RectTransform healthTransform;
+	public GameObject targetToFollow;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (targetToFollow != null) {
+			transform.position = new Vector2(targetToFollow.transform.position.x, targetToFollow.transform.position.y + 0.5f);
+		} else {
+
+			Destroy(gameObject);
+		}
+
+	}
+
+	public void setScale(float scale) {
+		healthTransform.localScale = new Vector2(scale, 1);
+	}
+
+}
