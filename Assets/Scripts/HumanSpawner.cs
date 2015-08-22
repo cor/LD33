6893d38c0ec3de454d40488b0fea21bef7fd96d6
cor <cor@pruijs.nl>
2015-08-22 11:15:36 +0200

    Add HumanSpawnPoint

---
+++ Assets/Scripts/HumanSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanSpawner : MonoBehaviour {
+
+	public float timeBetweenSpawns = 2f;
+	public GameObject human;
+
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnHuman", 0, timeBetweenSpawns);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SpawnHuman() {
+		Instantiate(human, transform.position, Quaternion.identity);
+	}
+
+	void SpawnHuman(Vector2 position) {
+		Instantiate(human, position, Quaternion.identity);
+	}
+
+
+}
