b444471b011c74711fa538ee49c111485f90e6ed
cor <cor@pruijs.nl>
2015-08-22 10:51:27 +0200

    Add Human Prefab, add HouseController

---
+++ Assets/Scripts/HouseController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseController : MonoBehaviour {
+
+	public int health = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
